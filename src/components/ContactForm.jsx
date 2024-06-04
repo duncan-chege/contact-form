58d0101e6953ef89f21b2b5640f10a55313e8816
viisualworks <dshege4@gmail.com>
2024-06-04 17:01:06 +0300

    add base files and styles

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,13 @@
+// Pay particular attention to making this form accessible. Building accessible forms is a key skill for front-end developers.
+
+import Styles from "./Styles.css"
+
+export default function ContactForm(){
+    return (
+        <main role="main">
+            <>
+            
+            </>
+        </main>
+    );
+}
