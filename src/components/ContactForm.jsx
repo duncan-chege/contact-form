0e640fd46bce4775ef388eed0d25fe1f00139f91
viisualworks <dshege4@gmail.com>
2024-06-06 15:40:35 +0300

    finish style and structure of the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,36 +6,47 @@ import "./Styles.css"
 export default function ContactForm(){
 return (
 <main role="main">
-    <h2> Contact Us </h2>
-    <form>
-        <div className="names-bloc">
-            <div>
-                <label htmlFor="fname" className="">First Name</label><br />
-                <input type="text" id="fname" name="first-name" className="" />
-            </div>
-            <div>
-                <label htmlFor="lname" className="">Last Name</label><br />
-                <input type="text" id="lname" name="last-name" className="" />
-            </div>
-        </div>
-        <div>
-            <label htmlFor="email" className="">Email</label><br />
-            <input type="email" id="email" name="email" className="" />
-        </div>
-        <div>
-            <p className="">Query Type</p>
-            <div className="radio-bloc">
+    <div className="form-bloc">
+        <h2> Contact Us </h2>
+        <form>
+            <div className="names-bloc">
                 <div>
-                    <input type="radio" id="general" name="queryType" value="General Enquiry" />
-                    <label htmlFor="general">General Enquiry</label>
+                    <label htmlFor="fname" className="">First Name</label><br />
+                    <input type="text" id="fname" name="first-name" className="" />
                 </div>
                 <div>
-                    <input type="radio" id="support" name="queryType" value="Support Request" />
-                    <label htmlFor="support">Support Request</label>
+                    <label htmlFor="lname" className="">Last Name</label><br />
+                    <input type="text" id="lname" name="last-name" className="" />
                 </div>
             </div>
-        </div>
-    </form>
+            <div>
+                <label htmlFor="email" className="">Email</label><br />
+                <input type="email" id="email" name="email" className="" />
+            </div>
+            <div>
+                <p className="">Query Type</p>
+                <div className="radio-bloc">
+                    <div>
+                        <input type="radio" id="general" name="queryType" value="General Enquiry" />
+                        <label htmlFor="general">General Enquiry</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="support" name="queryType" value="Support Request" />
+                        <label htmlFor="support">Support Request</label>
+                    </div>
+                </div>
+            </div>
+            <div className="message-bloc">
+                <label htmlFor="message" className="">Message</label><br />
+                <textarea id="message" name="message" rows="5"></textarea>
+            </div>
+            <div className="consent-bloc">
+                <input type="checkbox" id="consent" name="consent" value="Accept Consent" />
+                <label htmlFor="consent">I consent to being contacted by the team</label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 </main>
 );
 }
